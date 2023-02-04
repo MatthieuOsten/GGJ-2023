7c2c88413ad64a078f83f229b29bddcefbf136de
Matt <matthieuosten.0@gmail.com>
2023-02-04 21:23:25 +0100

    Add manager for game

---
+++ Assets/06-Scripts/Manager/GameManager.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region GAMESTATE
+    public enum GameState
+    {
+        Menu, // Menu phase just navigate in MainMenu 
+        Ingame, // Game is lanched
+
+        Tutorial, // Tutorial is now dont have the control or is limited
+        Paused, // Dont control player and other objects is stoped
+
+        Victory, // Everythings is stoped, next part of game is dicted by this state
+        Gameover, // Everythings is stoped, next part of game is dicted by this state 
+
+        IngameIntro, // The introGame is disable player controller but game is playing and show things to player
+        Cinematic // Control player is totally stoped and other object is stoped before end of cinematic possibly passed
+    }
+    #endregion
+
+    #region SINGLETON
+
+    /// <summary>
+    ///  Force a avoir qu'un seul GameManager
+    /// </summary>
+    [SerializeField] private static GameManager _instance = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+                // Si vrai, l'instance va etre cree
+                if (_instance == null)
+                {
+                    var newObjectInstance = new GameObject();
+                    newObjectInstance.name = typeof(GameManager).ToString();
+                    _instance = newObjectInstance.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    #region VARIABLE
+
+    [SerializeField] private GameState _state = GameState.IngameIntro;
+
+    #endregion
+
+    #region ASCESSEUR
+
+    public GameState State { 
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    #endregion
+
+    #region FUNCTION UNITY
+    #endregion
+
+    #region FUNCTION
+
+    public void ChangeFrameRate(int framerate)
+    {
+        Application.targetFrameRate = framerate;
+    }
+
+    /// <summary>
+    /// Change the scene
+    /// </summary>
+    /// <param name="name">Name of scene loaded</param>
+    static public void ChangeScene(string name)
+    {
+        if (!SceneIsExist(name)) { return; }
+
+        // If this scene is not entry scene also load new scene
+        if (SceneManager.GetActiveScene().name != name)
+            SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Change the scene but if for refresh actual scene also load this scene
+    /// </summary>
+    /// <param name="name">Name of scene loaded</param>
+    /// <param name="refreshScene">If True ignore the name scene is egal of this scene</param>
+    static public void ChangeScene(string name, bool refreshScene)
+    {
+        if (!SceneIsExist(name)) { return; }
+
+        // If this scene is not entry scene also load new scene
+        if (SceneManager.GetActiveScene().name != name || refreshScene)
+            SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Add scene to game 
+    /// </summary>
+    /// <param name="name">Nom de la scene rechercher</param>
+    static public void AdditiveScene(string name)
+    {
+        if (!SceneIsExist(name)) { return; }
+
+        if (SceneManager.GetSceneByName(name).isLoaded == false)
+            SceneManager.LoadScene(name, LoadSceneMode.Additive);
+    }
+
+    static private bool SceneIsExist(string name)
+    {
+        // Verify validity of name entry
+        if (SceneManager.sceneCountInBuildSettings > 1)
+        {
+            bool isValid = false;
+
+            for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+            {
+                string actualSceneName = SceneManager.GetSceneByBuildIndex(i).name;
+
+                // If scene name correspond also break loop and continue function else continue loop for search scene
+                if (actualSceneName == name) { isValid = true; break; }
+                else { Debug.Log("Scene " + actualSceneName + " is not " + name); }
+
+            }
+
+            // If is not valid return function
+            if (!isValid)
+            {
+                Debug.LogWarning("The scene " + name + " is not found.");
+                return false;
+            }
+        }
+        else
+        {
+            Debug.LogError("Build Setting dont have scene");
+            return false;
+
+        }
+
+        return true;
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+}
---
+++ Assets/06-Scripts/Manager/MenuManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    /// <summary>
+    /// List of Canvas of the scene 
+    /// </summary>
+    [Tooltip("The element 0 is the first display")]
+    [SerializeField] private List<GameObject> listOfCanvas = new List<GameObject>();
+
+    private void Start()
+    {
+        // Display first panel
+        ChangePanel(0);
+    }
+
+    /// <summary>
+    /// Recupere l'index dans "listOfPanels" du panneau designer par un nom par rapport au nom de l'objet
+    /// </summary>
+    /// <param name="index">Index encore inconnu</param>
+    /// <param name="name">Nom du canvas rechercer</param>
+    /// <returns>Retourne si le panneau as ete retrouver ou non</returns>
+    private bool TryGetPanelIndexFromName(out int index, string name)
+    {
+        // Recherche dans la liste des canvas
+        for (int i = 0; i < listOfCanvas.Count; i++)
+        {
+            // Si le nom du canvas correspond a l'entree alors le retourne et retourne vrai
+            if (listOfCanvas[i].name == name) {
+                index = i;
+                return true;
+            }
+        }
+
+        // Sinon l'index est egal a 0 et il retourne faux
+        index = 0;
+        return false;
+    }
+
+    /// <summary>
+    /// Change de panneau par rapport a son nom
+    /// </summary>
+    /// <param name="name">Nom du panneau que l'on veut afficher</param>
+    public void ChangePanel(string name)
+    {
+        int index = 0;
+
+        if (TryGetPanelIndexFromName(out index, name))
+        {
+            Debug.Log("Panel : " + name + " is actived");
+            ChangePanel(index);
+        }
+        else
+        {
+            Debug.LogWarning("Panel : " + name + " is not found");
+        }
+    }
+
+    /// <summary>
+    /// Change de panneau par rapport a son index dans "listOfPanels"
+    /// </summary>
+    /// <param name="index">Index du panneau dans la liste</param>
+    public void ChangePanel(int index)
+    {
+        if (listOfCanvas.Count == 0) {
+            Debug.LogWarning("List of panel is Empty");
+            return; 
+        }
+
+        for (int i = 0; i < listOfCanvas.Count; i++)
+        {
+            if (index == i) { listOfCanvas[i].SetActive(true); }
+            else { listOfCanvas[i].SetActive(false); }
+        }
+    }
+
+    /// <summary>
+    /// Change scene
+    /// </summary>
+    /// <param name="name">Name of new scene</param>
+    public void ChangeScene(string name)
+    {
+        GameManager.ChangeScene(name);
+    }
+
+    /// <summary>
+    /// Quit application of game
+    /// </summary>
+    public void QuitGame()
+    {
+        GameManager.Instance.QuitGame();
+    }
+
+}
