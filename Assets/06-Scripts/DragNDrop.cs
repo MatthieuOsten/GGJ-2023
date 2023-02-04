4fde89f7f9546c10d4445f118a581e843c23849b
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-04 11:50:05 +0100

    WIP: added drag but not the drop

---
+++ Assets/06-Scripts/DragNDrop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNDrop : MonoBehaviour
+{
+    [Header("Position")]
+    [SerializeField] private float _mouseZ = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    private void OnMouseDrag()
+    {
+        Vector3 mousepos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _mouseZ);
+        transform.position = Camera.main.ScreenToWorldPoint(mousepos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
