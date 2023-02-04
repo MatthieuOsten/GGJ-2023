9127b1aa73e71a9153accc940fc45b382fc1d3a9
Matt <matthieuosten.0@gmail.com>
2023-02-04 17:17:26 +0100

    System of spawn customer

---
+++ Assets/06-Scripts/Customer/CustomerManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    [Space(5), Header("SPAWN"), Space(5)]
+    [SerializeField] private Transform[] _pointsOfSpawn = new Transform[0];
+    [SerializeField] private bool[] _spawnIsAvailible = new bool[0];
+
+    [Header("COOLDOWN")]
+    [SerializeField] private Timer _timerSpawn;
+
+    [Space(5), Header("PREFABS"), Space(5)]
+    [SerializeField] private GameObject[] _prefabsCustomers = new GameObject[0];
+    [SerializeField] private List<GameObject[]> _transformsCustomers = new List<GameObject[]>();
+
+    private void Start()
+    {
+        GenerateCustomers();
+    }
+
+    private void Update()
+    {
+        // Spawn customer after intervale
+        if (_timerSpawn.Update())
+        {
+            ActivateCustomer();
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        // Set number of bool from spawn on point of spawn list
+      if (_pointsOfSpawn.Length != _spawnIsAvailible.Length) { _spawnIsAvailible = new bool[_pointsOfSpawn.Length]; }  
+    }
+#endif
+
+    /// <summary>
+    /// Active a random customer
+    /// </summary>
+    private void ActivateCustomer()
+    {
+        // Get customer
+        int indexCustomer = Random.Range(0, _prefabsCustomers.Length);
+
+        GameObject actualCustomer = GetInactiveCustomer(indexCustomer);
+
+        if (actualCustomer == null) { return; }
+
+        // Get availible spawn of customer
+        List<Transform> availibleSpawn = new List<Transform>();
+
+        for (int i = 0; i < _pointsOfSpawn.Length; i++)
+        {
+            if (i < _spawnIsAvailible.Length && _pointsOfSpawn[i] != null)
+            {
+                if (_spawnIsAvailible[i])
+                {
+                    availibleSpawn.Add(_pointsOfSpawn[i]);
+                }
+            }
+        }
+
+        // Teleport Customer on point of spawn
+
+        actualCustomer.transform.position = availibleSpawn[Random.Range(0, availibleSpawn.Count)].position;
+        actualCustomer.SetActive(true);
+
+    }
+
+    /// <summary>
+    /// Generate customers on gameobject 
+    /// </summary>
+    private void GenerateCustomers()
+    {
+        _transformsCustomers = new List<GameObject[]>();
+
+        _spawnIsAvailible = new bool[_pointsOfSpawn.Length];
+        for (int i = 0; i < _spawnIsAvailible.Length; i++)
+        {
+            _spawnIsAvailible[i] = true;
+        }
+
+        // Create the number of list from number of prefabs
+        for (int i = 0; i < _prefabsCustomers.Length; i++)
+        {
+            // Initialise tab from number of spawn point
+            _transformsCustomers.Add(new GameObject[_pointsOfSpawn.Length]);
+
+            for (int j = 0; j < _pointsOfSpawn.Length; j++)
+            {
+                _transformsCustomers[i][j] = Instantiate(_prefabsCustomers[i], transform.position, transform.rotation, transform);
+                _transformsCustomers[i][j].SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get active customer from index of variant
+    /// </summary>
+    /// <param name="index">index of prefabs</param>
+    /// <returns></returns>
+    private GameObject GetInactiveCustomer(int index)
+    {
+        foreach (var customer in _transformsCustomers[index])
+        {
+            if (customer.gameObject.activeSelf)
+            {
+                return customer;
+            }
+        }
+
+        return null;
+    }
+
+}
---
+++ Assets/06-Scripts/Tools/Editor/TimerDrawer.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Timer))]
+public class TimerDrawer : PropertyDrawer
+{
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Using BeginProperty / EndProperty on the parent property means that
+        // prefab override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Calculate rects
+        int padding = 5;
+        int valueWidthPartTime = (int)(position.width / 2.5);
+
+        int valueWidthFinishLabel = 35,
+            valueWidthPlayLabel = 35,
+            valueWidthToggle = 10;
+
+        int relativeWidthField = valueWidthPartTime / 3,
+            relativeWidthFinishLabel = (valueWidthPartTime - (relativeWidthField + (valueWidthToggle * 2))) / 2,
+            relativeWidthPlayLabel = valueWidthFinishLabel - 20;
+
+        int finalWidthPlayLabel = (relativeWidthPlayLabel > valueWidthPlayLabel) ? relativeWidthPlayLabel : valueWidthPlayLabel,
+            finalWidthFinishLabel = (relativeWidthFinishLabel > valueWidthFinishLabel) ? relativeWidthFinishLabel : valueWidthFinishLabel;
+
+        float valuePositionX = position.x;
+
+        // Draw label
+        if (position.width > 250)
+        {
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            valueWidthPartTime = (int)(position.width / 2.5);
+
+            relativeWidthField = valueWidthPartTime / 3;
+            relativeWidthFinishLabel = (valueWidthPartTime - (relativeWidthField + (valueWidthToggle * 2))) / 2;
+            relativeWidthPlayLabel = valueWidthFinishLabel - 20;
+
+            finalWidthPlayLabel = (relativeWidthPlayLabel > valueWidthPlayLabel) ? relativeWidthPlayLabel : valueWidthPlayLabel;
+            finalWidthFinishLabel = (relativeWidthFinishLabel > valueWidthFinishLabel) ? relativeWidthFinishLabel : valueWidthFinishLabel;
+
+            valuePositionX = position.x;
+        }
+        else if (position.width > 150)
+        {
+            var nameRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            EditorGUI.LabelField(nameRect, "Timer ");
+            valuePositionX += relativeWidthField + padding;
+        }
+        else if (position.width > 100)
+        {
+            var nameRect = new Rect(valuePositionX, position.y, 10, position.height);
+            EditorGUI.LabelField(nameRect, "T ");
+            valuePositionX += 10 + padding;
+        }
+
+
+        if (position.width > 250)
+        {
+
+            var timeRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + padding;
+            var timePassedRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + padding;
+            var timerToRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + 15;
+
+            var playLabelRect = new Rect(valuePositionX, position.y, finalWidthPlayLabel, position.height);
+            valuePositionX += valueWidthPlayLabel - 10 + padding;
+            var playRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+            valuePositionX += valueWidthToggle + padding;
+            var finishLabelRect = new Rect(valuePositionX, position.y, finalWidthFinishLabel, position.height);
+            valuePositionX += valueWidthFinishLabel + padding;
+            var finishRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.FloatField(timeRect, property.FindPropertyRelative("_time").floatValue);
+            EditorGUI.FloatField(timePassedRect, property.FindPropertyRelative("_timePassed").floatValue);
+            EditorGUI.EndDisabledGroup();
+            property.FindPropertyRelative("_timer").floatValue = EditorGUI.FloatField(timerToRect, property.FindPropertyRelative("_timer").floatValue);
+            EditorGUI.LabelField(playLabelRect, "Play ");
+            property.FindPropertyRelative("_play").boolValue = EditorGUI.Toggle(playRect, property.FindPropertyRelative("_play").boolValue);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.LabelField(finishLabelRect, "Finish ");
+            EditorGUI.Toggle(finishRect, property.FindPropertyRelative("_finish").boolValue);
+            EditorGUI.EndDisabledGroup();
+        }
+        else if (position.width > 150)
+        {
+            finalWidthPlayLabel = 10;
+            finalWidthFinishLabel = 10;
+
+            // Calculate rects
+            var timeRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + padding;
+            var timePassedRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + padding;
+            var timerToRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + 15;
+
+            var playLabelRect = new Rect(valuePositionX, position.y, finalWidthPlayLabel, position.height);
+            valuePositionX += finalWidthPlayLabel + padding;
+            var playRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+            valuePositionX += valueWidthToggle + padding;
+            var finishLabelRect = new Rect(valuePositionX, position.y, finalWidthFinishLabel, position.height);
+            valuePositionX += finalWidthFinishLabel + padding;
+            var finishRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.FloatField(timeRect, property.FindPropertyRelative("_time").floatValue);
+            EditorGUI.FloatField(timePassedRect, property.FindPropertyRelative("_timePassed").floatValue);
+            EditorGUI.EndDisabledGroup();
+            property.FindPropertyRelative("_timer").floatValue = EditorGUI.FloatField(timerToRect, property.FindPropertyRelative("_timer").floatValue);
+            EditorGUI.LabelField(playLabelRect, "P ");
+            property.FindPropertyRelative("_play").boolValue = EditorGUI.Toggle(playRect, property.FindPropertyRelative("_play").boolValue);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.LabelField(finishLabelRect, "F ");
+            EditorGUI.Toggle(finishRect, property.FindPropertyRelative("_finish").boolValue);
+            EditorGUI.EndDisabledGroup();
+        }
+        else if (position.width > 100)
+        {
+            finalWidthPlayLabel = 10;
+
+            // Calculate rects
+            var timeRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + padding;
+            var timerToRect = new Rect(valuePositionX, position.y, relativeWidthField, position.height);
+            valuePositionX += relativeWidthField + 15;
+
+            var playLabelRect = new Rect(valuePositionX, position.y, finalWidthPlayLabel, position.height);
+            valuePositionX += finalWidthPlayLabel + padding;
+            var playRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+            valuePositionX += valueWidthToggle + padding;
+            var finishRect = new Rect(valuePositionX, position.y, valueWidthToggle, position.height);
+
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.FloatField(timeRect, property.FindPropertyRelative("_time").floatValue);
+            EditorGUI.EndDisabledGroup();
+            property.FindPropertyRelative("_timer").floatValue = EditorGUI.FloatField(timerToRect, property.FindPropertyRelative("_timer").floatValue);
+            EditorGUI.LabelField(playLabelRect, "P ");
+            property.FindPropertyRelative("_play").boolValue = EditorGUI.Toggle(playRect, property.FindPropertyRelative("_play").boolValue);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.Toggle(finishRect, property.FindPropertyRelative("_finish").boolValue);
+            EditorGUI.EndDisabledGroup();
+        }
+
+        EditorGUI.EndProperty();
+    }
+}
+
+#endif
