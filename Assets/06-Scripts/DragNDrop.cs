ce44303aad4d7a8e03c4f1d4cd4c8bf97e4e7c3b
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-04 19:15:06 +0100

    added great part of the structure of the game

--- Assets/06-Scripts/DragNDrop.cs
+++ Assets/06-Scripts/DragNDrop.cs
@@ -7,27 +7,38 @@ public class DragNDrop : MonoBehaviour
 {
     [Header("Position")]
     [SerializeField] private float _mouseZ = 5f;
-    [SerializeField] private Vector3 _initialPos;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-    
+
     private void OnMouseDrag()
     {
         Vector3 mousepos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _mouseZ);
         transform.position = Camera.main.ScreenToWorldPoint(mousepos);
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            if (other.gameObject.CompareTag("Bag"))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        transform.position = new Vector3(_initialPos.x, _initialPos.y, _initialPos.z);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
---
+++ Assets/06-Scripts/Generator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private int _nbOfGameObject = 10;
+    [SerializeField] private Vector3 _spawnPoint;
+    private GameObject[] _allGameObject = new GameObject[0];
+    public bool isDragged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDragged = true;
+        _allGameObject = new GameObject[_nbOfGameObject];
+        
+        for (int i = 0; i != _nbOfGameObject; i++)
+        {
+            _allGameObject[i] = Instantiate(_gameObject).gameObject;
+            _allGameObject[i].SetActive(false);
+        }
+        _allGameObject[0].SetActive(true);
+        _allGameObject[0].transform.position = _spawnPoint;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        int temp = 0;
+        
+        for (int i = 0; i != _nbOfGameObject; i++)
+        {
+            if (_allGameObject[i].activeSelf)
+            {
+                temp += 1;
+            }
+
+            if (temp == 0)
+            {
+                _allGameObject[0].SetActive(true);
+                _allGameObject[0].transform.position = _spawnPoint;
+            }
+        }
+    }
+}
