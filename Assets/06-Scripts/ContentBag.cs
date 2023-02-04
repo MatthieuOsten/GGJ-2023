f9b0c007e90f2b067182f270cb78429a89d3c804
Marlon PEGAHI <marlon.pegahi@epitech.eu>
2023-02-04 21:57:46 +0100

    added product in the bag system

---
+++ Assets/06-Scripts/ContentBag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class ContentBag : MonoBehaviour
+{
+    private List<string> _productsList = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Product"))
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                string temp = "";
+                for (int i = 0; other.gameObject.name[i] != '('; i++)
+                {
+                    temp += other.gameObject.name[i];
+                }
+                _productsList.Add(temp);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/06-Scripts/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewProduct",menuName = "Product")]
+public class Product : ScriptableObject
+{
+    public String name;
+    public Sprite sprite;
+}
