c69c1e2e31508f992302bde02a6219f038bbcc76
Matt <matthieuosten.0@gmail.com>
2023-02-05 14:26:25 +0100

    Update Highscore

--- Assets/06-Scripts/Score/HighScore.cs
+++ Assets/06-Scripts/Score/HighScore.cs
@@ -1,11 +1,209 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewHighScore", menuName = "HighScore")]
 public class HighScore : ScriptableObject
 {
-    public float first_score;
-    public float second_score;
-    public float third_score;
+    [SerializeField] private Score _lastScore;
+    [SerializeField] private List<Score> _scores;
+
+    [System.Serializable]
+    public class Score
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private int _id;
+        [SerializeField] private float _score;
+        [SerializeField] private DateTime _time;
+
+        public string Name { get { return _name; } }
+        public int Id { get { return _id; } }
+        public float ScoreValue { 
+            get { return _score; } 
+            set { _score = (value < 0) ? 0 : value; }
+        }
+
+        public DateTime Time { 
+            get { return _time; } 
+            set { _time = value; }
+        }
+
+        public Score(string name, int id,float score, DateTime time)
+        {
+            _name = name;
+            _score = score;
+            _time = time;
+            _id = id;
+
+            ScoreValue = score;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} | {1} as score of {2} make in {3}", Id, Name, ScoreValue,Time.ToString());
+        }
+    }
+
+    public Score LastScore
+    {
+        get { return _lastScore; }
+        set {
+            _lastScore = new Score(value.Name,-1,value.ScoreValue,DateTime.Now); 
+        }
+    }
+
+    public List<Score> AllData
+    { 
+        get { return _scores; }
+    }
+
+    public List<float> AllScores {
+        get {
+            List<float> floats= new List<float>();
+
+            foreach (Score score in AllData)
+            {
+                floats.Add(score.ScoreValue);
+            }
+
+            return floats; 
+        }
+    }
+
+    public Score[] TopThree
+    {
+        get
+        {
+            Score[] tempTab = new Score[3];
+
+            UpdateOrderOfHighScore();
+            tempTab[0] = AllData[AllData.Count - 1];
+            tempTab[1] = AllData[AllData.Count - 2];
+            tempTab[2] = AllData[AllData.Count - 3];
+
+            return tempTab;
+        }
+    }
+
+    public void UpdateOrderOfHighScore()
+    {
+        string debugList = "List -> ";
+        foreach (var item in _scores)
+        {
+            debugList += item.ScoreValue.ToString() + " | ";
+        }
+        Debug.Log(debugList);
+
+        int n = _scores.Count;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (_scores[j].ScoreValue < _scores[minIndex].ScoreValue)
+                {
+                    minIndex = j;
+                }
+            }
+            Score temp = _scores[minIndex];
+            _scores[minIndex] = _scores[i];
+            _scores[i] = temp;
+        }
+
+        debugList = "List -> ";
+        foreach (var item in _scores)
+        {
+            debugList += item.ScoreValue.ToString() + " | ";
+        }
+        Debug.Log("Tri : " + debugList);
+    }
+
+    public void AddNewScore(string name, float score)
+    {
+        Score newScore = new Score(name, GetNewID(), score, System.DateTime.Now);
+        Debug.Log(newScore.ToString());
+        _scores.Add(newScore);
+    }
+
+    public void AddNewScore(float score)
+    {
+        Score newScore = new Score("Default", GetNewID(), score, System.DateTime.Now);
+        Debug.Log(newScore.ToString());
+        _scores.Add(newScore);
+    }
+
+    public void AddNewScore(Score newScore)
+    {
+        newScore = new Score(newScore.Name, GetNewID(), newScore.ScoreValue, System.DateTime.Now);
+        Debug.Log(newScore.ToString());
+        _scores.Add(newScore);
+    }
+
+    public void AddNewScore()
+    {
+        Score newScore = new Score(LastScore.Name, GetNewID(), LastScore.ScoreValue, System.DateTime.Now);
+        Debug.Log(newScore.ToString());
+        _scores.Add(newScore);
+    }
+
+    private int GetNewID()
+    {
+        int id = 0;
+
+        foreach (var item in AllData)
+        {
+            if (item.Id > id)
+            {
+                id = item.Id;
+            }
+        }
+
+        return id++;
+    }
+
+    private Score GetTopScore()
+    {
+        Score tempScore = new Score("NULL", -1,0, DateTime.Now);
+
+        foreach (var score in AllData)
+        {
+            if (score.ScoreValue > tempScore.ScoreValue)
+            {
+                return score;
+            }
+        }
+
+        return tempScore;
+    }
+
+    private Score GetTopScore(float limit)
+    {
+        Score tempScore = new Score("NULL", -1, 0, DateTime.Now);
+
+        foreach (var score in AllData)
+        {
+            if (score.ScoreValue > tempScore.ScoreValue && score.ScoreValue < limit)
+            {
+                return score;
+            }
+        }
+
+        return tempScore;
+    }
+
+    private int GetEgalsScoreCount(float comparingScore)
+    {
+        int nbrOfEgals = 0;
+
+        foreach (var score in AllScores)
+        {
+            if (score == comparingScore) { nbrOfEgals++; }
+        }
+
+        return nbrOfEgals;
+    }
+
 }
---
+++ Assets/06-Scripts/Score/WidgetScore.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class WidgetScore : MonoBehaviour
+{
+    private float _score;
+    private bool _animate_text;
+    private float _timer = 0.25f;
+    [SerializeField] private TMP_Text _TextScore;
+    [SerializeField] private TMP_Text _TextHighscore;
+    [SerializeField] private HighScore _scorehig;
+    
+    void Start()
+    {
+        _score = 0;
+        _animate_text = false;
+        DisableHighscore();
+    }
+
+    private void Update()
+    {
+        if (_animate_text)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                _TextScore.fontSize = 70;
+                _animate_text = false;
+                _timer = 0.25f;
+            }
+        }
+
+        DisplayTopThree();
+
+    }
+
+    public void Add_score()
+    {
+        _score += 1;
+        _TextScore.text = _score.ToString();
+        _animate_text = true;
+        _TextScore.fontSize = 100;
+    }
+
+    private void DisplayTopThree()
+    {
+        if (_TextHighscore != null)
+        {
+            string text = "Highscore\n";
+             text += "Top 1 : " + _scorehig.TopThree[0].ToString() + "\n";
+             text += "Top 2 : " + _scorehig.TopThree[1].ToString() + "\n";
+             text += "Top 3 : " + _scorehig.TopThree[2].ToString() + "\n";
+
+            _TextHighscore.text = text;
+        }
+    }
+
+    //public void CheckHighScore()
+    //{
+    //    if (_score > _scorehig.TopThree[0])
+    //    {
+    //        _scorehig.third_score = _scorehig.second_score;
+    //        _scorehig.second_score = _scorehig.first_score;
+    //        _scorehig.first_score = _score;
+    //    }
+    //    else if (_score > _scorehig.second_score)
+    //    {
+    //        _scorehig.third_score = _scorehig.second_score;
+    //        _scorehig.second_score = _score;
+    //    }
+    //    else if (_score > _scorehig.third_score)
+    //    {
+    //        _scorehig.third_score = _score;
+    //    }
+    //}
+
+    public void UpdateLastScore()
+    {
+        _scorehig.LastScore.ScoreValue = _score;
+    }
+
+    public void AddDebug()
+    {
+        _scorehig.AddNewScore("Debug",UnityEngine.Random.Range(-99,1000));
+    }
+
+    public void AddLastScore()
+    {
+        UpdateLastScore();
+        _scorehig.AddNewScore(_scorehig.LastScore);
+        _scorehig.UpdateOrderOfHighScore();
+    }
+
+    public void EnableScore()
+    {
+        _TextScore.gameObject.SetActive(true);
+    }
+    
+    public void DisableScore()
+    {
+        _TextScore.gameObject.SetActive(false);
+    }
+    public void EnableHighscore()
+    {
+        _TextHighscore.gameObject.SetActive(true);
+    }
+    
+    public void DisableHighscore()
+    {
+        _TextHighscore.gameObject.SetActive(false);
+    }
+    public void Update_highscore()
+    {
+        _TextHighscore.text = "HighScore\n\n1. " + _scorehig.TopThree[0].ToString() + "\n2. " + _scorehig.TopThree[1].ToString() + "\n3. " + _scorehig.TopThree[2].ToString();
+    }
+}
